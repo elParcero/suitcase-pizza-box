baddc9f9d1afa677959b4c0f86cf72e0dc85d3a2
Jorge Diaz <jorge7soccer@gmail.com>
2018-07-24 09:22:20 -0400

    ENH: Added legacy handlers.

---
+++ suitcase/pizza_box/handlers_legacy.py
@@ -0,0 +1,46 @@
+from collections import namedtuple
+
+class PizzaBoxEncHandlerTxt(HandlerBase):
+    encoder_row = namedtuple('encoder_row',
+                             ['ts_s', 'ts_ns', 'encoder', 'index', 'state'])
+    "Read PizzaBox text files using info from filestore."
+    def __init__(self, fpath, chunk_size):
+        self.chunk_size = chunk_size
+        with open(fpath, 'r') as f:
+            self.lines = list(f)
+
+    def __call__(self, chunk_num):
+        cs = self.chunk_size
+        return [self.encoder_row(*(int(v) for v in ln.split()))
+                for ln in self.lines[chunk_num*cs:(chunk_num+1)*cs]]
+
+
+class PizzaBoxDIHandlerTxt(HandlerBase):
+    di_row = namedtuple('di_row', ['ts_s', 'ts_ns', 'encoder', 'index', 'di'])
+    "Read PizzaBox text files using info from filestore."
+    def __init__(self, fpath, chunk_size):
+        self.chunk_size = chunk_size
+        with open(fpath, 'r') as f:
+            self.lines = list(f)
+
+    def __call__(self, chunk_num):
+        cs = self.chunk_size
+        return [self.di_row(*(int(v) for v in ln.split()))
+                for ln in self.lines[chunk_num*cs:(chunk_num+1)*cs]]
+
+
+class PizzaBoxAnHandlerTxt(HandlerBase):
+    encoder_row = namedtuple('encoder_row', ['ts_s', 'ts_ns', 'index', 'adc'])
+    "Read PizzaBox text files using info from filestore."
+
+    bases = (10, 10, 10, 16)
+    def __init__(self, fpath, chunk_size):
+        self.chunk_size = chunk_size
+        with open(fpath, 'r') as f:
+            self.lines = list(f)
+
+    def __call__(self, chunk_num):
+
+        cs = self.chunk_size
+        return [self.encoder_row(*(int(v, base=b) for v, b in zip(ln.split(), self.bases)))
+                for ln in self.lines[chunk_num*cs:(chunk_num+1)*cs]]
